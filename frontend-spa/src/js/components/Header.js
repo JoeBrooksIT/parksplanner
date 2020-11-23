4f71a93d93e7178546de475a0dbf5e1dc7453c66
blahablast <37739821+blahablast@users.noreply.github.com>
2020-11-23 13:44:54 -0500

    added css and img folder. Header is displaying

--- frontend-spa/src/js/components/Header.js
+++ frontend-spa/src/js/components/Header.js
@@ -1,5 +1,5 @@
-export default Header = () => {
+export default function Header() {
   return `
-    <h1>Header</h1>
+    <h1>Header1</h1>
   `
 }
--- frontend-spa/src/js/main.js
+++ frontend-spa/src/js/main.js
@@ -1,1 +1,10 @@
 import Header from "./components/Header"
+
+export default () => {
+  header()
+}
+
+function header() {
+  const header = document.querySelector(".header")
+  header.innerHTML = Header()
+}
