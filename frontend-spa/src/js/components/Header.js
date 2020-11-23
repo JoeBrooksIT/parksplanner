6393d718f0eb68cc771fde969820b39175f5899c
blahablast <37739821+blahablast@users.noreply.github.com>
2020-11-23 12:00:46 -0500

    created components folder and added a header

---
+++ frontend-spa/src/js/components/Header.js
@@ -0,0 +1,5 @@
+export default Header = () => {
+  return `
+    <h1>Header</h1>
+  `
+}
--- frontend-spa/src/js/main.js
+++ frontend-spa/src/js/main.js
@@ -1,7 +1,1 @@
-export default function(){
-    document.querySelector('#app').innerHTML=
-    `
-    <h2>Your Awesome API Collection</h2>
-    <h3>It's gonna be great!</h3>
-    `
-}
+import Header from "./components/Header"
