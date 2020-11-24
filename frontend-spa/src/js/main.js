c36f5b1ba30a9e53aee3a56865fff6326aa713c8
blahablast <37739821+blahablast@users.noreply.github.com>
2020-11-24 09:37:09 -0500

    setting up get request

--- frontend-spa/src/js/main.js
+++ frontend-spa/src/js/main.js
@@ -1,3 +1,6 @@
+const API_KEY = "yLyZjsEKQn7yqwa5Ejn0yNBAxbH604yYhN95sMCs"
+const API_URL = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${API_KEY}`
+
 import Header from "./components/Header"
 
 export default () => {
@@ -8,3 +11,5 @@ function header() {
   const header = document.querySelector(".header")
   header.innerHTML = Header()
 }
+
+function getParks() {}
