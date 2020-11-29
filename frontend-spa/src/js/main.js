8cf7e7d4b8009178a5384b8696c0b649704533df
Bremen Mask <thejames0023@gmail.com>
2020-11-29 12:37:28 -0500

    added error, form, results component, added watch form function, display results function and get parks function

--- frontend-spa/src/js/main.js
+++ frontend-spa/src/js/main.js
@@ -1,27 +1,82 @@
-const API_KEY = "yLyZjsEKQn7yqwa5Ejn0yNBAxbH604yYhN95sMCs"
-const API_URL = `https://developer.nps.gov/api/v1/parks?api_key=${API_KEY}`
-
-import Header from "./components/Header"
-import Parks from "./components/Parks"
+import Header from "./components/Header";
+import Parks from "./components/Parks";
 
 export default () => {
-  header()
-  getParks()
-}
+  header();
+  getParks();
+};
 
-const main = document.querySelector(".main-content")
+const main = document.querySelector(".main-content");
 
 function header() {
-  const header = document.querySelector(".header")
-  header.innerHTML = Header()
+  const header = document.querySelector(".header");
+  header.innerHTML = Header();
 }
 
-function getParks() {
-  fetch(API_URL)
-    .then(res => res.json())
-    .then(parks => {
-      console.log(parks)
-      main.innerHTML = Parks(parks.data)
-    })
-    .catch(err => console.log(err))
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params).map(
+    (key) => `${[encodeURIComponent(key)]}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
 }
+
+// function getParks() {
+//   fetch(API_URL)
+//     .then(res => res.json())
+//     .then(parks => {
+//       console.log(parks)
+//       main.innerHTML = Parks(parks.data)
+//     })
+//     .catch(err => console.log(err))
+// }
+
+function getParks(API_URL, stateArr, maxResults, API_KEY) {
+  const params = {
+    stateCode: stateArr,
+    limit: maxResults,
+  };
+
+  const queryString = formatQueryParams(params);
+  const url = API_URL + "?" + queryString + "&api_key=" + API_KEY;
+  console.log(url);
+
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusTest);
+    })
+    .then((responseJson) => displayResults(responseJson, maxResults))
+    .catch((err) => {
+      ".error".text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  ".form".addEventListener("submit", function () {
+    event.preventDefault();
+    const API_URL = "https://developer.nps.gov/api/v1/parks";
+    const stateArr = ".state-entered".value().split(",");
+    const maxResults = ".result-amt".value();
+
+    const API_KEY = "yLyZjsEKQn7yqwa5Ejn0yNBAxbH604yYhN95sMCs";
+    getParks(API_URL, stateArr, maxResults, API_KEY);
+  });
+}
+
+function displayResults(respnseJson, maxResults) {
+  console.log(responseJson);
+
+  (".error" === ""(".results-list")) === "";
+
+  for (let i = 0; (i < responseJson.data.length) & (i < maxResults); i++) {
+    ".results-list"
+      .append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+    <p>{responseJson.data[i].description}</p>
+    </li>`);
+  }
+  ".results".removeClass("hidden");
+}
+
+watchForm();
