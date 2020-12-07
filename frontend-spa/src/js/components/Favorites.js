d14ddca46d2a69a7a2f94f6a7ea9750f7df12d7b
blahablast <37739821+blahablast@users.noreply.github.com>
2020-12-07 14:57:38 -0500

    improved the favorites button

--- frontend-spa/src/js/components/Favorites.js
+++ frontend-spa/src/js/components/Favorites.js
@@ -1,18 +1,18 @@
 export default function Favorites(parks) {
-  console.log(parks);
+  console.log(parks)
   return `
-  <h1>Favorites</h1>
+  <h1 class="button">Favorites</h1>
   <ol>
   ${parks
-    .map((park) => {
+    .map(park => {
       return `
       <li class="favorites__name">
         <p>${park.parkName}</p>
         <button class="user__delete__park" id="${park.id}">Remove from favorites</button>
       </li>
-      `;
+      `
     })
     .join("")}
   </ol>
-  `;
+  `
 }
