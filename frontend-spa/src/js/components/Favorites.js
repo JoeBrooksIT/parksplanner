47ce8ee3d709fd6055de83f036cd0c143f807215
blahablast <37739821+blahablast@users.noreply.github.com>
2020-12-08 11:13:38 -0500

    favorites are displaying and centered

--- frontend-spa/src/js/components/Favorites.js
+++ frontend-spa/src/js/components/Favorites.js
@@ -1,18 +1,19 @@
 export default function Favorites(parks) {
   console.log(parks)
   return `
-  <h1 class="button">Favorites</h1>
-  <ol>
-  ${parks
-    .map(park => {
-      return `
-      <li class="favorites__name">
-        <p>${park.parkName}</p>
-        <button class="user__delete__park" id="${park.id}">Remove from favorites</button>
-      </li>
-      `
-    })
-    .join("")}
-  </ol>
+  <div class="container fave-container">
+    <ol>
+    ${parks
+      .map(park => {
+        return `
+        <li class="favorites__name">
+          <h2>${park.parkName}</h2>
+          <button class="user__delete__park" id="${park.id}">Remove from favorites</button>
+        </li>
+        `
+      })
+      .join("")}
+    </ol>
+  </div>
   `
 }
