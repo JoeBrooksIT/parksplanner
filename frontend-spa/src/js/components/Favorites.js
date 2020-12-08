a2c605cf32d1a2af9106887e2b4ebac9c0aca8c6
Bremen Mask <thejames0023@gmail.com>
2020-12-08 15:10:39 -0500

    added trip date function but it is not functioning, removed form tag from favorites component

--- frontend-spa/src/js/components/Favorites.js
+++ frontend-spa/src/js/components/Favorites.js
@@ -8,8 +8,16 @@ export default function Favorites(parks) {
         return `
         <li class="favorites__name">
           <h2>${park.parkName}</h2>
+          <div class="date-container">
+              <div>
+                <label for="date-input">Date</label>
+                <input type="text" name="trip-date" id="dateInput" />
+              </div>
+              <input type="button" value="Submit Date" class="date-button">
+          </div> 
           <button class="user__delete__park" id="${park.id}">Remove from favorites</button>
         </li>
+        <hr>
         `
       })
       .join("")}
