553a0d086841b86de0c61302ad20a38a511a1171
Bremen Mask <thejames0023@gmail.com>
2020-12-09 10:48:57 -0500

    backend trip date, thirrd migration, date button hooked up

--- backend/ParksPlanner/ParksPlanner/Migrations/20201209152927_AddedTripDate.Designer.cs
+++ backend/ParksPlanner/ParksPlanner/Migrations/20201209152927_AddedTripDate.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ParksPlanner;
 
 namespace ParksPlanner.Migrations
 {
     [DbContext(typeof(ParksContext))]
-    partial class ParksContextModelSnapshot : ModelSnapshot
+    [Migration("20201209152927_AddedTripDate")]
+    partial class AddedTripDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
